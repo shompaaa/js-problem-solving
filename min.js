186dad89ea85496795259f09031e5eee18746b2f
shompaaa <shompaakter334@gmail.com>
2026-05-17 15:14:15 +0600

    min number function

---
+++ min.js
@@ -0,0 +1,14 @@
+const phonePrices = [15000,25000,32000,175000,22000,220000, 12000]
+
+function getMin(prices){
+    let min = phonePrices[0]
+    for(const price of prices){
+        if(price < min){
+            min = price
+        }
+    }
+    return min;
+}
+
+const cheap = getMin(phonePrices)
+console.log('The cheapest price of phone is:', cheap);
